8cf824a9c6103e7e35367d6984b03c142311de1f
KhairienNissa <cacakhairien@gmail.com>
2022-06-20 20:24:23 +0700

    update

--- src/components/category.js
+++ src/components/category.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from "react";
 import {useNavigate } from "react-router-dom";
-import axios from "axios";
+import Axios from "axios";
 
 
 const Category = () => {
@@ -8,28 +8,30 @@ const Category = () => {
     const [category, setCategory] = useState([])
 
 
-    const getCategory = async () => {
-        try {
-          const { data } = await axios.get(
-            "https://asia-southeast2-sejutacita-app.cloudfunctions.net/fee-assessment-categories"
-          );
-          console.log(data);
-        //   setCategory(data.results);
-        } catch (error) {
-          console.log(error);
-        }
-      };
+    // const getCategory = async () => {
+    //     try {
+    //       const { data } = await axios.get(
+    //         "https://asia-southeast2-sejutacita-app.cloudfunctions.net/fee-assessment-categories"
+    //       );
+    //       console.log(data);
+    //     //   setCategory(data.results);
+    //     } catch (error) {
+    //       console.log(error);
+    //     }
+    //   };
 
      useEffect (() =>{
-        // Axios.get('https://asia-southeast2-sejutacita-app.cloudfunctions.net/fee-assessment-categories')
-        // .then(result => {
-        //     console.log('data api', result)
-
-        // })
-        // .catch(err => {
-        //     console.log('error', err)
-        // })
-        getCategory()
+        Axios.get('https://asia-southeast2-sejutacita-app.cloudfunctions.net/fee-assessment-categories')
+        .then(result => {
+            console.log('data api', result.data)
+            const response = result.data;
+            setCategory(response)
+            
+        })
+        .catch(err => {
+            console.log('error', err)
+        })
+      
 
     }, [])
 
